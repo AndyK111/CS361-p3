624a0ea3dbcd4ea043fa7317b8bb8838b78a833d
AndyK111 <AndyKempf@u.boisestate.edu>
2026-04-27 20:36:59 -0600

    TMSimulator complete

--- tm/TMSimulator.java
+++ tm/TMSimulator.java
@@ -1,7 +1,22 @@
 package tm;
 
-public class TMSimulator {
-    
+import java.io.IOException;
+
+public class TMSimulator
+{
+    public static void main(String[] args) throws IOException
+    {
+        if (args.length != 1)
+        {
+            System.err.println("Usage: java tm.TMSimulator <input-file-path>");
+            return;
+        }
+
+        TM turingMachine = TMParser.parse(args[0]);
+
+        turingMachine.run();
+        System.out.println(turingMachine.getTapeContents());
+    }
 
 
 }
