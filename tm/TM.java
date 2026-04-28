29d2df237664e20fc09cb64c0ebdde902a3e2f2e
AndyK111 <AndyKempf@u.boisestate.edu>
2026-04-27 20:20:32 -0600

    TM implementation completed.

--- tm/TM.java
+++ tm/TM.java
@@ -1,4 +1,45 @@
 package tm;
-public class TM {
-    
+
+@SuppressWarnings("unused")
+public class TM
+{
+    protected TMState[] states;
+    protected TMState currentState;
+    protected TMState haltState;
+    protected TMTape tape;
+
+    public TM(TMState[] states, TMTape tape)
+    {
+        this.states = states;
+        this.currentState = states[0];
+        this.haltState = states[states.length-1];
+        this.tape = tape;
+    }
+
+    public void run()
+    {
+        while (currentState != haltState)
+        {
+            int readSymbol = tape.read();
+            TMTransition transition = currentState.getTransition(readSymbol);
+
+            tape.write(transition.getWrite());
+
+            if (transition.getDirection() == TMDirection.LEFT)
+            {
+                tape.moveLeft();
+            }
+            else
+            {
+                tape.moveRight();
+            }
+
+            currentState = transition.getDestination();
+        }
+    }
+
+    public String getTapeContents()
+    {
+        return tape.getVisitedContents();
+    }
 }
