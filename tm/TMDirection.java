a1634a770de9299625d479d7d612c023523ec431
AndyK111 <AndyKempf@u.boisestate.edu>
2026-04-29 23:20:37 -0600

    TMDirection Enum javadocs!

--- tm/TMDirection.java
+++ tm/TMDirection.java
@@ -1,10 +1,19 @@
 package tm;
 
+/**
+ * Enum for giving names to tape directions.
+ * @author Andy Kempf
+ */
 public enum TMDirection
 {
     RIGHT,
     LEFT;
 
+    /**
+     * Parse L, R string into tape direction enum values
+     * @param direction String representation of tape direction ("L" & "R")
+     * @return Enum value corresponding to String tape direction.
+     */
     public static TMDirection parseDirection(String direction)
     {
         if (direction.equals("L")) return LEFT;
